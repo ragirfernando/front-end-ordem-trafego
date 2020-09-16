0c74fe8f9e3a9b29260074b4bca1850ecc25d52b
Ragir Fernando Aparecido da Silva <ragirfernando@gmail.com>
2020-09-16 17:53:33 -0300

    Imagem para o carousel.

--- src/service/config.js
+++ src/service/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const http = axios.create({
-    baseURL: 'https://ragir-ordem-trafego.herokuapp.com/api/'
-    /*baseURL: 'http://localhost:8080/api/'*/
+    /*baseURL: 'https://ragir-ordem-trafego.herokuapp.com/api/'*/
+    baseURL: 'http://localhost:8082/api/'
 })
